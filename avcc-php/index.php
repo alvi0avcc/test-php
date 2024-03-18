047b0560b8c024ba27ecb79ce3c8cc03c2b38745
Aleksandr <127627433+alvi0avcc@users.noreply.github.com>
2024-03-18 16:46:57 +0300

    +

---
+++ avcc-php/index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="css/index.css" rel="stylesheet">
+</head>
+
+<body>
+    
+    <div class = 'body'>
+        <div class = 'header'>
+        </div>
+
+        <div class = 'main'>
+        </div>
+        
+        <div class = 'footer'>
+        </div>    
+    </div>
+
+</body>
+
+</hmtl>
